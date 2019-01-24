8c0ce261e3d1906bbb20eb7b4bfc3365b6dcab28
Paul Uncangco <islandertron@gmail.com>
2019-01-23 23:57:45 -0500

    Styled Components

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import Header from '../components/Header';
-import Meta from '../components/Meta';
+import Header from './Header';
+import Meta from './Meta';
+
 class Page extends Component {
-    render() {
-        return (
-            <div>
-                <Meta/>
-                <Header/>
-                {this.props.children}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Meta />
+        <Header />
+        {this.props.children}
+      </div>
+    );
+  }
 }
 
 export default Page;
